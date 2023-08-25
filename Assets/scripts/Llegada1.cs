20756068dd51ee081fd7b96bba372af92436ef57
Andres Camilo Zambrano Garcia <andrescamilozambranogarcia@MacBook-Air-de-Andres.local>
2023-08-24 20:10:24 -0500

    Level one

---
+++ Assets/scripts/Llegada1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Llegada1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if(collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Nivel2");
+        }
+    }
+}
